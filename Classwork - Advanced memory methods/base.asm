cfdb94338ba6ed74f6e96abbbe4d7cb4c9551c65
Ithamar <57110382+IthamarBaron@users.noreply.github.com>
2025-02-02 10:58:02 +0200

    scan for spaces
    REP and SCAS

---
+++ Classwork - Advanced memory methods/base.asm
@@ -0,0 +1,58 @@
+
+Data Segment
+	
+	string db "a bb cc "
+	stringLength = $ - string
+
+Data Ends
+Code Segment
+Assume Cs:Code, Ds:Data
+
+; on start BX,SI point to the start of the word.
+; SI advances to the next delimiter, 
+; on hit (found delimiter) SI swaps delimiter with $ for print 
+; print using BX 
+; mov bx to point to the start of the next word
+
+
+start: 
+Mov Ax,Data
+Mov Ds,Ax
+Mov Es, Ax
+
+	cld             
+	lea di, string
+	mov  bx, di
+	MOV  CX, stringLength   
+search:
+	MOV  AL, ' '
+	REPNE SCASB      
+	JnZ  not_found  
+    ; found
+	dec di
+	mov [di], byte ptr '$'
+	
+	;add $
+	MOV DX, BX
+	MOV AH, 09h
+	INT 21h 
+
+	;/n
+	MOV DL, 0Dh
+	MOV AH, 02h
+	INT 21h
+	;/r
+	MOV DL, 0Ah
+	MOV AH, 02h
+	INT 21h
+	
+	inc di
+	mov bx, di
+	
+jmp search
+
+not_found:
+mov ax, 4c00h
+Int 21h
+Code Ends
+End start
